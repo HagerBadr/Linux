5632b9da8117265400de26735a1a054c0ec52989
HagerBadr <hagerbadr463@gmail.com>
2023-05-10 15:03:32 +0300

    Create PythonSlack.py

---
+++ 01-python/LEC1_Task/PythonSlack.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def send_slack_message(payload, webhook):
+    """Send a Slack message to a channel via a webhook. 
+    
+    Args:
+        payload (dict): Dictionary containing Slack message, i.e. {"text": "This is a test"}
+        webhook (str): Full Slack webhook URL for your chosen channel. 
+    
+    Returns:
+        HTTP response code, i.e. <Response [503]>
+    """
+
+    return requests.post(webhook, json.dumps(payload))
+
+webhook = "https://hooks.slack.com/services/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"
+payload = {"text": "Hello This is Hager Badr"}
+send_slack_message(payload, webhook)
